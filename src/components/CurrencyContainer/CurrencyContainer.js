b53c2ed52aacc3e6229b72fbe51d7c523d0033f9
Gianluca Bragaglia <gianluca@Gianlucas-Mac-mini.local>
2018-04-26 01:16:43 +0200

    Initial commit

---
+++ src/components/CurrencyContainer/CurrencyContainer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class CurrencyContainer extends React.Component {
+
+    render() {
+
+        return (
+
+            <div>
+                <h1>GBP/JPY</h1>
+                <p>152.462</p>
+            </div>
+        )
+    }
+}
+
+export default CurrencyContainer;
