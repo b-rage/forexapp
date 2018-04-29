8f016d965585db1b0d32479ab3f867ed8a9a8c81
Gianluca Bragaglia <gianluca@Gianlucas-Mac-mini.local>
2018-04-29 02:32:53 +0200

    x

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+/* import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -18,4 +18,51 @@ class App extends Component {
   }
 }
 
-export default App;
+export default App; */
+
+
+import React, { Component } from 'react';
+import './css/App.css';
+import logo from './logo.svg';
+import List from './components/List';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+      items: []
+    };
+  }
+
+  onChange = (event) => {
+    
+      this.setState({ term: event.target.value });
+ 
+    
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      term: '',
+      items: [...this.state.items, this.state.term]
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <form className="App" onSubmit={this.onSubmit}>
+          <input id='inputDo' value={this.state.term} onChange={this.onChange} />
+          <button className="btn waves-effect waves-light" type="submit" name="action">Submit</button>
+        </form>
+        <List items={this.state.items} />
+      </div>
+    );
+  }
+}
---
+++ src/components/List.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import '../css/List.css';
+
+const List = props => (
+  <ul className="collection" id='listDo'>
+    {
+      props.items.map((item, index) => <li className="collection-item" key={index}>{item}</li>)
+    }
+  </ul>
+);
+
+export default List;
